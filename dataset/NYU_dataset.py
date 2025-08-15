26c011dad5c6f54ce3d6d349327f9cf3b8e461d7
Ali Youssef <ali97_mohamed@hotmail.com>
2025-08-15 22:35:52 +0300

    ali

--- dataset/NYU_dataset.py
+++ dataset/NYU_dataset.py
@@ -18,12 +18,13 @@ class NYUDataset(Dataset):
         self.mode = mode
         self.max_depth = max_depth
         self.teacher_model = teacher_model.lower()
-        
+        if isinstance(crop_size, int): crop_size = (crop_size, crop_size)
+        self.crop_size = crop_size
+
         # Student (DINOv2) processor setup
         # Everything is Default except for do_resize we set it as False
         self.dino_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
         self.dino_processor.do_center_crop = True
-        if isinstance(crop_size, int): crop_size = (crop_size, crop_size)
         self.dino_processor.crop_size = {'height': crop_size[0], 'width': crop_size[1]}
         self.dino_processor.do_resize = False
         
@@ -59,6 +60,14 @@ class NYUDataset(Dataset):
     def __len__(self):
         return len(self.nyu_data)
     
+    def center_crop(self, image):
+        h, w = image.shape[-2:]
+        crop_h, crop_w = self.crop_size
+        top = (h - crop_h) // 2
+        left = (w - crop_w) // 2
+        cropped_image = image[:, top : top + crop_h, left: left + crop_w]
+        return cropped_image
+    
     def __getitem__(self, idx):
         image = self.data_dir + self.nyu_data[idx][0].removeprefix("data/")
         depth = self.data_dir + self.nyu_data[idx][1].removeprefix("data/")
@@ -70,6 +79,7 @@ class NYUDataset(Dataset):
             # For DepthAnything, we use the image directly
             teacher_input = self.teacher_processor(images=image,
                                                    return_tensors="pt").pixel_values.squeeze(0)
+            teacher_input = self.center_crop(teacher_input)
         else:
             # For Dinov2, we use the depth map and stack it to match RGB channels
             if self.mode == 'train':
@@ -83,9 +93,9 @@ class NYUDataset(Dataset):
             teacher_input = teacher_input.repeat(3, 1, 1)  # Repeat to match RGB channels
             teacher_input = self.teacher_processor(images=teacher_input,
                                                    return_tensors="pt").pixel_values.squeeze(0)
-
+        
         return {'student_input': student_input,
-                'teacher_input': teacher_input}
+            'teacher_input': teacher_input}
 
 
 if __name__ == "__main__":
@@ -98,6 +108,20 @@ if __name__ == "__main__":
     for batch in dataloader:
         student_input = batch['student_input']
         teacher_input = batch['teacher_input']
-        print(f"Student Shape: {student_input.shape}")
-        print(f"Teacher Shape: {teacher_input.shape}")
+        student_input = student_input.squeeze().permute(1,2,0)
+        teacher_input = teacher_input.squeeze().permute(1,2,0)
+        import matplotlib.pyplot as plt
+        fig, axs = plt.subplots(1, 2, figsize=(18, 6))
+
+        axs[0].imshow(student_input)
+        axs[0].set_title("student_input")
+        axs[0].axis("off")
+        
+        axs[0].imshow(teacher_input)
+        axs[0].set_title("teacher_input")
+        axs[0].axis("off")
+
+        plt.tight_layout()
+        plt.savefig("nyu_sample.png", dpi=300)
+        print("Figure saved as 'nyu_sample.png'")
         break
